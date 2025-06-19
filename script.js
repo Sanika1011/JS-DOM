77f64ed57fa4961c269dbca791e623908661eea4
Sanika1011 <desaisanika501@gmail.com>
2025-06-19 22:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.querySelector("p").innerText = "added a new text!!";
 
 
 let para = document.querySelector("p");
-// para.setAttribute("class","newclass");
+para.setAttribute("class","newclass");
 para.classList.add("newcontent");
 
 
@@ -54,26 +54,26 @@ document.querySelector("body").prepend(btn);
 // Qs. Create a H2 heading element with text - “Hello JavaScript”. Append “from Apna College
 //  students” to this text using JS
 
-// const { createElement } = require("react");
+ const { createElement } = require("react");
 
-// let h2 = document.querySelector("h2");
-// h2.innerText = h2.innerText + " from college student";
-// console.log(h2.innerText);
+ let h2 = document.querySelector("h2");
+ h2.innerText = h2.innerText + " from college student";
+ console.log(h2.innerText);
 
 
 
 //  Qs. Create 3 divs with common class name - “box”. Access them & add some unique text to each
 //  of them.
 
-// let divs = document.querySelectorAll(".box");
-// let idx = 1;
+ let divs = document.querySelectorAll(".box");
+ let idx = 1;
 
-// for(div of divs) {
-//     div.innerText = `new value div ${idx}`;
-//     idx++;
-// }
+ for(div of divs) {
+     div.innerText = `new value div ${idx}`;
+     idx++;
+ }
 
 
-// divs[0].innerText = "new value div 1st";
-// divs[1].innerText = "new value div 2nd";
-// divs[2].innerText = "new value div 3rd";
+divs[0].innerText = "new value div 1st";
+divs[1].innerText = "new value div 2nd";
+divs[2].innerText = "new value div 3rd";
